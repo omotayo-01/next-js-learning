284742ab038a31019f99f089d79bc202c52a6ee2
omotayo-01 <oooladokun27@student.lautech.edu.ng>
2026-04-14 21:24:02 +0100

    Initial commit

---
+++ app/blog/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <div className="space-y-6">
+      <h2 className="text-xl font-semibold">Individual Blog Here</h2>
+    </div>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blogs() {
+  return (
+    <div className="space-y-6">
+      <h2 className="text-xl font-semibold">Blogs will come here</h2>
+    </div>
+  );
+}
