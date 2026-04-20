00f761345ad7d562d68f96e12aad84ff4b95ac8b
omotayo-01 <oooladokun27@student.lautech.edu.ng>
2026-04-20 14:44:42 +0100

    pdated the [id]/page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function Blog() {
+import blogs from "@/data/blog.json";
+
+export default function BlogPost({ params }: { params: { id: string } }) {
+  const post = blogs.find((p) => p.id === params.id);
+
+  if (!post) return <div>Post not found</div>;
+
   return (
     <div className="space-y-6">
-      <h2 className="text-xl font-semibold">Individual Blog Here</h2>
+      <h2 className="text-xl font-semibold">{post.title}</h2>
+      <p>{post.content}</p>
     </div>
   );
 }
